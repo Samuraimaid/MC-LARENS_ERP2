e91281860fd58f94c30c32adfb8d59d1c9de6983
Samuraimaid <133938639+Samuraimaid@users.noreply.github.com>
2026-05-08 18:13:07 -0600

    chore: add generated env and backup snapshots

--- frontend/public/env.js
+++ frontend/public/env.js
@@ -1,9 +1,9 @@
 // This file is auto-generated at build time
 window.__API_BASE__ = '/api';
 window.__ATTENDANCE_KIOSK_SHORTCUT_PIN__ = '';
-window.__BUILD_TIME__ = '2026-05-08T20:53:50.290Z';
-window.__BUILD_ID__ = '20260508-1453';
+window.__BUILD_TIME__ = '2026-05-08T22:41:34.233Z';
+window.__BUILD_ID__ = '20260508-1641';
 window.__BUILD_VERSION__ = '0.2.0-beta.0';
-window.__BUILD_GIT_HASH__ = '3824906';
+window.__BUILD_GIT_HASH__ = '701ef10';
 window.__BUILD_GIT_BRANCH__ = 'master';
-window.__BUILD_LABEL__ = '0.2.0-beta.0 | 20260508-1453 | 3824906 @master';
+window.__BUILD_LABEL__ = '0.2.0-beta.0 | 20260508-1641 | 701ef10 @master';
